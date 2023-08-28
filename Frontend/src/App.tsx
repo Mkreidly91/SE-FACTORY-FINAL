75c3750fe3cfaec47d56d83ce5b183717262ab78
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-28 15:12:51 +0300

    Frontend: added boiler plate vite app, cleaned up coded, installed tailwind, added some icons/images

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app sora">
+      <h1 className="text-2xl font-extrabold ">Hellooo</h1>
+    </div>
+  );
+}
+
+export default App;
