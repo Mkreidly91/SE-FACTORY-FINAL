e488c856fae14f692da7d8dfc232d4fc90a25ae2
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-29 16:03:11 +0300

    Frontend: changed logo size in navbar, added gradient border to buttons in landing

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { FC } from 'react';
 const Navbar: FC = () => {
   return (
     <div className="flex justify-between items-center py-8 px-10 text-white bg-transparent z-10  ">
-      <img src={logo} alt="" />
+      <img src={logo} alt="" className="w-[100px]" />
       <div className="nav-button-container flex items-center  gap-24">
         <Button text="ABOUT" />
         <Button text="SERVICE" />
