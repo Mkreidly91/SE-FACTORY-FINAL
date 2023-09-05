53a4c30c6816643f99d49c000495310c5abb3ddd
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-05 13:00:58 +0300

    Backend: Created apartment model

---
+++ Backend/src/models/apartment.ts
@@ -0,0 +1,21 @@
+import { Schema, model, Document } from 'mongoose';
+import { PanoramaSchema, IPanorama } from './panorama';
+
+interface IApartment {
+  name: string;
+  description: string;
+  url?: string;
+  panoramas?: IPanorama[];
+}
+interface IApartmentDocument extends IApartment, Document {}
+
+const ApartmentSchema = new Schema<IApartment>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  url: { type: String, required: true },
+  panoramas: [PanoramaSchema],
+});
+
+const Apartment = model<IApartmentDocument>('Apartment', ApartmentSchema);
+
+export { IApartment, IApartmentDocument, ApartmentSchema, Apartment };
