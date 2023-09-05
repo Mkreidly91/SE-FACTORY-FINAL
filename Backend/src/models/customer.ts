f197ef58860908cbc7101eb95ae964b75a8d75fe
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-05 13:00:06 +0300

    Backend: Created customer model

--- Backend/src/models/customer.ts
+++ Backend/src/models/customer.ts
@@ -1,11 +1,16 @@
 import mongoose, { Schema, model, Document } from 'mongoose';
 
-interface ICustomer {
+interface IUser {
   name: string;
   email: string;
   password: string;
 }
 
+interface ICustomer extends IUser {
+  profilePic?: string;
+}
+
+interface ICustomerDocument extends ICustomer, Document {}
 const CustomerSchema = new Schema<ICustomer>({
   name: { type: String, required: true },
   email: {
@@ -17,8 +22,9 @@ const CustomerSchema = new Schema<ICustomer>({
     match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}/,
   },
   password: { type: String, required: true, minlength: 6 },
+  profilePic: { type: String },
 });
 
-const Customer = model('Customer', CustomerSchema);
+const Customer = model<ICustomerDocument>('Customer', CustomerSchema);
 
-export default Customer;
+export { IUser, ICustomer, Customer };
