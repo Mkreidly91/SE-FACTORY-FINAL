ce7c232244904f39b211be2d39acec433e501b5e
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-06 19:30:54 +0300

    Backend: Installed UUID library, used it to generate uuids for filenames

--- Backend/src/helpers/s3.helpers.ts
+++ Backend/src/helpers/s3.helpers.ts
@@ -1,10 +1,14 @@
 import {
   DeleteObjectCommand,
+  DeleteObjectsCommand,
+  DeleteObjectsCommandInput,
   HeadBucketCommand,
   PutObjectCommand,
   S3Client,
 } from '@aws-sdk/client-s3';
 import envConfig from '../configs/env.config';
+import { v4 as uuidv4 } from 'uuid';
+
 // function getUrlFromBucket(s3Bucket: any, fileName: string) {
 //   const {
 //     config: { params, region },
@@ -12,8 +16,17 @@ import envConfig from '../configs/env.config';
 //   const regionString = region.includes('us-east-1') ? '' : '-' + region;
 //   return `https://${params.Bucket}.s3${regionString}.amazonaws.com/${fileName}`;
 // }
+function getFileExtension(fileName: string) {
+  return `${fileName.split('.').pop()}`;
+}
+
 function getFileUrl(fileName: string) {
   return `https://${envConfig.BUCKET_NAME}s3.${envConfig.BUCKET_REGION}.amazonaws.com/${fileName}`;
+}
+
+function getFileNameFromUrl(url: string) {
+  const pathParts = url.split('/');
+  return pathParts[pathParts.length - 1];
 }
 
 const s3 = new S3Client({
@@ -29,7 +42,7 @@ const checkBucket = async (bucket: string) => {
 const upload = async (fileData: Express.Multer.File) => {
   const params = {
     Bucket: envConfig.BUCKET_NAME,
-    Key: fileData.originalname,
+    Key: `${uuidv4()}.${getFileExtension(fileData.originalname)}`,
     Body: fileData.buffer,
   };
   const res = await s3.send(new PutObjectCommand(params));
@@ -37,7 +50,9 @@ const upload = async (fileData: Express.Multer.File) => {
   return getFileUrl(params.Key);
 };
 
-const deleteFileService = async (key: string) => {
+const deleteFile = async (url: string) => {
+  const key = getFileNameFromUrl(url);
+  console.log(key);
   const params = {
     Bucket: envConfig.BUCKET_NAME,
     Key: key,
@@ -48,4 +63,25 @@ const deleteFileService = async (key: string) => {
   return response && response.$metadata.httpStatusCode === 204;
 };
 
-export { getFileUrl, checkBucket, upload, deleteFileService };
+const deleteBulk = async (urls: string[]) => {
+  const objectsToDelete = urls.map((url) => ({ Key: getFileNameFromUrl(url) }));
+  const params: DeleteObjectsCommandInput = {
+    Bucket: envConfig.BUCKET_NAME,
+    Delete: {
+      Objects: objectsToDelete,
+    },
+  };
+
+  const response = await s3.send(new DeleteObjectsCommand(params));
+
+  return response && response.$metadata.httpStatusCode === 204;
+};
+
+export {
+  getFileUrl,
+  getFileNameFromUrl,
+  checkBucket,
+  upload,
+  deleteFile,
+  deleteBulk,
+};
