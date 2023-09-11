37e1b4c5af5c89996b0d37c397cb4e0a097995d6
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-11 10:57:48 +0300

    Frontend: Integrated react router for app and dashboard

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,30 @@
 import './App.css';
+import TestRHF from './pages/TestRHF';
 import Landing from './pages/landing/Landing';
 import Test from './pages/sample3d/test';
+import DashBoard from './components/Dashboard/Dashboard';
+import DashboardRoutes from './components/Dashboard/DashboardRoutes';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app sora h-full">
+    <div className="app sora h-full w-full">
       {/* <Landing /> */}
-      <Test />
+
+      {/* <TestRHF /> */}
+
+      <Routes>
+        <Route path="/test" element={<Test />} />
+        <Route
+          path="/dashboard/*"
+          element={
+            <DashBoard>
+              {/* <DashboardRoutes />
+              <ProjectForm /> */}
+            </DashBoard>
+          }
+        />
+      </Routes>
     </div>
   );
 }
