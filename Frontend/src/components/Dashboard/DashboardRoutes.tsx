c6d440c69c8e74df5611ac961dc3a0350242eb2b
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-11 10:58:45 +0300

    Frontend: Created routes for dashboard links

---
+++ Frontend/src/components/Dashboard/DashboardRoutes.tsx
@@ -0,0 +1,19 @@
+// DashboardRoutes.js
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ProjectForm from '../Forms/ProjectForm';
+import Projects from '../../pages/Company/Dashboard/Projects';
+import AddOrEditProject from '../../pages/Company/Dashboard/AddOrEditProject';
+
+const DashboardRoutes = () => {
+  return (
+    <Routes>
+      <Route index path="/projects" element={<Projects />} />
+      <Route path="projects/addProject" element={<AddOrEditProject />} />
+      {/* <Route path="/dashboard/settings" element={DashboardSettings} /> */}
+      {/* Add more dashboard-specific routes */}
+    </Routes>
+  );
+};
+
+export default DashboardRoutes;
