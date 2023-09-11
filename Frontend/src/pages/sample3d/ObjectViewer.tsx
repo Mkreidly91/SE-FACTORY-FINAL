0b2e735d95584407008d8af1fba0e4c50e5434e6
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-11 11:09:31 +0300

    Frontend: Created a 3d object viewer component

---
+++ Frontend/src/pages/sample3d/ObjectViewer.tsx
@@ -0,0 +1,60 @@
+import { Html, OrbitControls, useGLTF } from '@react-three/drei';
+import { Canvas, ThreeEvent } from '@react-three/fiber';
+import React, { useState, useEffect, FC, Suspense, useRef } from 'react';
+import * as THREE from 'three';
+
+export const ObjectViewer = ({ url }: { url: string }) => {
+  const m: any = useGLTF(url);
+  const aspect = window.innerWidth / window.innerHeight;
+  const d = 60;
+  const camera = new THREE.OrthographicCamera(
+    -d * aspect,
+    d * aspect,
+    d,
+    -d,
+    1,
+    1000
+  );
+  camera.position.set(60, 60, 60);
+  camera.zoom = 80;
+
+  return (
+    <div className="w-[80%] h-[500px] mx-auto">
+      <Canvas
+        className=" border h-full bg-transparent"
+        frameloop="demand"
+        camera={camera}
+        dpr={window.devicePixelRatio}
+      >
+        <ambientLight intensity={0.1} />
+        <directionalLight color="white" position={[0, 0, 5]} intensity={1} />
+        {/* <directionalLight color="white" position={[5, 0, 5]} /> */}
+        <pointLight intensity={2} position={[-10, 30, 20]} castShadow />
+        <pointLight
+          intensity={1}
+          position={[20, 30, -10]}
+          castShadow
+          visible={true}
+        />
+        {/* <spotLight color="yellow" position={[0, 0, 0]} /> */}
+
+        <OrbitControls makeDefault target={[0, 2, 0]} />
+        <Suspense>
+          {m && (
+            <primitive
+              onPointerDown={(e) => {
+                console.log(e.normal);
+                //   placeMarker(e);
+              }}
+              object={m.scene}
+            >
+              {/* {children} */}
+            </primitive>
+          )}
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+};
+
+export default ObjectViewer;
