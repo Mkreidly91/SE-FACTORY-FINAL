efc8149b62c8992e15e18a88296809302f3fa89c
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-11 11:12:54 +0300

    Frontend: Started with models and panorama form for company dashboard

---
+++ Frontend/src/components/Forms/ModelsAndPanoramaForm.tsx
@@ -0,0 +1,13 @@
+import ObjectViewer from '../../pages/sample3d/ObjectViewer';
+import FileUploadForm from '../Common/FileUploadForm';
+import MultiFileUpload from '../Common/MultiFileUpload';
+
+const ModelsAndPanoramaForm = () => {
+  return (
+    <>
+      <MultiFileUpload />
+    </>
+  );
+};
+
+export default ModelsAndPanoramaForm;
