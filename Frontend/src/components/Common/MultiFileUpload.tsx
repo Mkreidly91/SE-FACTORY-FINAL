85ea778541f9d0be37e6908fcec8732b9091240c
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-12 10:24:47 +0300

    Frontend:fixed styling in multifile upload component

--- Frontend/src/components/Common/MultiFileUpload.tsx
+++ Frontend/src/components/Common/MultiFileUpload.tsx
@@ -64,7 +64,7 @@ function MultiFileUpload() {
   };
 
   return (
-    <div className="bg-gray-500 h-screen w-screen sm:px-8 md:px-16 sm:py-8">
+    <div className="bg-gray-500 h-screen w-full sm:px-8 md:px-16 sm:py-8">
       <main className="container mx-auto max-w-screen-lg h-full">
         {/* file upload modal */}
         <article
