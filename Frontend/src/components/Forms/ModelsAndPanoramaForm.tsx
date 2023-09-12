e4394fbcf1798d2668310828dd67a047f0e1dc73
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-12 17:17:27 +0300

    Frontend: Created the base for models and panorama form page

--- Frontend/src/components/Forms/ModelsAndPanoramaForm.tsx
+++ Frontend/src/components/Forms/ModelsAndPanoramaForm.tsx
@@ -1,11 +1,29 @@
 import ObjectViewer from '../../pages/sample3d/ObjectViewer';
+import Button from '../Common/Button';
 import FileUploadForm from '../Common/FileUploadForm';
 import MultiFileUpload from '../Common/MultiFileUpload';
 
 const ModelsAndPanoramaForm = () => {
   return (
-    <div className="w-full h-full">
-      <MultiFileUpload />
+    <div className="w-full h-full mt-20 pb-5 flex flex-col items-center ">
+      <div className="flex flex-col sm:px-8 md:px-16 sm:py-8  min-w-[80%] ">
+        <div className="label-container flex mb-5 ">
+          <span className="color-blue-light ">Add 3d Model</span>
+        </div>
+        <MultiFileUpload />
+      </div>
+      <div className="flex flex-col sm:px-8 md:px-16 sm:py-8 min-w-[80%]">
+        <div className="label-container flex   mb-5 ">
+          <span className="color-blue-light ">Add Panoramic Images</span>
+        </div>
+        <MultiFileUpload />
+      </div>
+      <div className="marker-button-container flex justify-center">
+        <Button
+          text="Set Markers and Hotspots"
+          className="color-blue-light w-fit mx-auto cursor-pointer"
+        />
+      </div>
     </div>
   );
 };
