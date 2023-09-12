3b67cf2ab5951a9d80376266310e68a4117a0764
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-12 10:18:24 +0300

    Frontend:Styled projects page for company dashboard added a link to edit project

--- Frontend/src/pages/Company/Dashboard/Projects.tsx
+++ Frontend/src/pages/Company/Dashboard/Projects.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import ProjectCard from '../../../components/Common/ProjectCard';
 import { getProjects } from '../../../api/company.api';
+import { Link } from 'react-router-dom';
+import AddIcon from '@mui/icons-material/Add';
 
 const Projects = () => {
-  console.log('Projects rendered');
   const [projects, setProjects] = useState([]);
   useEffect(() => {
     const fetchProjects = async () => {
@@ -13,24 +14,33 @@ const Projects = () => {
     fetchProjects();
   }, []);
 
-  console.log(projects);
   return (
-    <>
-      <span>Hello</span>
-      {projects &&
-        projects.map(
-          (p: { name: string; description: string; thumbnail: string }) => {
+    <div className="projects-page-container flex flex-col gap-10  w-full h-full monster mt-20 px-10  py-16 ">
+      <div className="projecs-page-header flex justify-between">
+        <span className=" text-3xl font-light">Projects</span>
+        <Link to={'addProject'}>
+          <div className="flex items-center gap-2 py-[10px] px-[30px] border border-[#2499E3] rounded-lg">
+            <AddIcon sx={{ color: '#2499E3' }} />
+            <button className="color-blue-light ">Add New</button>
+          </div>
+        </Link>
+      </div>
+      <div className="projects-card-container flex gap-5 ">
+        {projects &&
+          projects.map((p: any) => {
             const { name, description, thumbnail } = p;
             return (
-              <ProjectCard
-                title={name}
-                description={description}
-                img={thumbnail}
-              />
+              <Link to={`addProject/${p._id}`} state={p}>
+                <ProjectCard
+                  title={name}
+                  description={description}
+                  img={thumbnail}
+                />
+              </Link>
             );
-          }
-        )}
-    </>
+          })}
+      </div>
+    </div>
   );
 };
 
