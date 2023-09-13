a2a51c67dba50f17753c7410040469424349e7c2
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-13 16:37:34 +0300

    Frontend: Added rooute for company homepage

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,7 +5,7 @@ import Test from './pages/sample3d/test';
 import DashBoard from './components/Dashboard/Dashboard';
 import DashboardRoutes from './components/Dashboard/DashboardRoutes';
 import { Routes, Route } from 'react-router-dom';
-
+import CompanyHome from './pages/Company/CompanyHome';
 function App() {
   return (
     <div className="app sora h-full w-full">
@@ -15,6 +15,7 @@ function App() {
 
       <Routes>
         <Route path="/test" element={<Test />} />
+        <Route path="/companyHome" element={<CompanyHome />} />
         <Route
           path="/dashboard/*"
           element={
