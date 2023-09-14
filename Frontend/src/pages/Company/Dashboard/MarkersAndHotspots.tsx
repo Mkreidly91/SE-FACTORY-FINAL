0d15c1281ec973d57c1eec026b921ae77d7a7e3b
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-14 15:23:29 +0300

    Frontend: testing on panorama component

--- Frontend/src/pages/Company/Dashboard/MarkersAndHotspots.tsx
+++ Frontend/src/pages/Company/Dashboard/MarkersAndHotspots.tsx
@@ -1,29 +1,30 @@
 import { Suspense } from 'react';
 import ImageSlider from '../../../components/Common/ImageSlider/ImageSlider';
 import ObjectViewer from '../../sample3d/ObjectViewer';
-import V360 from '../../sample3d/V360';
+import V360 from '../../sample3d/V360.tsx';
 
 const MarkersAndHotspots = ({ initialState }) => {
   const objectUrl = initialState.apartments[0].url;
-  const imageUrl = initialState.apartments[0].panoramas[0].url;
+  // const imageUrl = initialState.apartments[0].panoramas[0].url;
   const panoramas = initialState.apartments[0].panoramas.map((e) => e.url);
-  console.log(objectUrl);
-  console.log(initialState);
+
   return (
-    <Suspense>
-      <div className="mt-20">
-        {objectUrl && <ObjectViewer url={initialState.apartments[0].url} />}
-        {imageUrl && (
-          <V360 image={initialState.apartments[0].panoramas[0].url} />
-        )}
-        {panoramas && (
-          <ImageSlider
-            images={panoramas}
-            imageCardStyles="aspect-[16/9] h-auto"
-          />
-        )}
-      </div>
-    </Suspense>
+    <>
+      <Suspense>
+        <div className="mt-20">
+          {objectUrl && <ObjectViewer url={initialState.apartments[0].url} />}
+          <div className="w-[70%] m-auto ">
+            <V360 image={''} />
+          </div>
+          {panoramas && (
+            <ImageSlider
+              images={panoramas}
+              imageCardStyles="aspect-[16/9] h-auto"
+            />
+          )}
+        </div>
+      </Suspense>
+    </>
   );
 };
 
