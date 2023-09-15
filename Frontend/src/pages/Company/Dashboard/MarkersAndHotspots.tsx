c43d90332320ba20ed734f9f282a11b30175511f
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-15 17:34:25 +0300

    Frontend: Addede PanoEditor to MarkersAndHotspots Page

--- Frontend/src/pages/Company/Dashboard/MarkersAndHotspots.tsx
+++ Frontend/src/pages/Company/Dashboard/MarkersAndHotspots.tsx
@@ -2,17 +2,18 @@ import { Suspense } from 'react';
 import ImageSlider from '../../../components/Common/ImageSlider/ImageSlider';
 import ObjectViewer from '../../sample3d/ObjectViewer';
 import V360 from '../../sample3d/V360.tsx';
+import PanoEditor from '../../sample3d/PanoEditor.tsx';
 
 const MarkersAndHotspots = ({ initialState }) => {
-  const objectUrl = initialState.apartments[0].url;
+  const objectUrl = initialState.url;
   // const imageUrl = initialState.apartments[0].panoramas[0].url;
-  const panoramas = initialState.apartments[0].panoramas.map((e) => e.url);
-
+  const panoramas = initialState.panoramas.map((e) => e.url);
+  console.log(initialState);
   return (
     <>
       <Suspense>
         <div className="mt-20">
-          {objectUrl && <ObjectViewer url={initialState.apartments[0].url} />}
+          {objectUrl && <ObjectViewer url={initialState.url} />}
           <div className="w-[70%] m-auto ">
             <V360 image={''} />
           </div>
@@ -20,6 +21,14 @@ const MarkersAndHotspots = ({ initialState }) => {
             <ImageSlider
               images={panoramas}
               imageCardStyles="aspect-[16/9] h-auto"
+            />
+          )}
+        </div>
+        <div className="w-[70%] m-auto ">
+          {panoramas && initialState.panoramas && (
+            <PanoEditor
+              initial={initialState.panoramas[1]}
+              panoramas={initialState.panoramas}
             />
           )}
         </div>
