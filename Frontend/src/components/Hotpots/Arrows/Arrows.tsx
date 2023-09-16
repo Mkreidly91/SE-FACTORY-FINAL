7496ec35e0559e2c1377f88eb8157289a9ac8e0b
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-16 19:26:41 +0300

    Frontend: Added edit state for arrows hotspot component

--- Frontend/src/components/Hotpots/Arrows/Arrows.tsx
+++ Frontend/src/components/Hotpots/Arrows/Arrows.tsx
@@ -7,6 +7,8 @@ interface hotSpotProps {
   scale?: string;
   className?: String;
   edit?: boolean;
+  info?: string;
+  onEditClick?: () => void;
 }
 
 const Arrows = ({
@@ -14,8 +16,10 @@ const Arrows = ({
   pitch,
   onClick,
   className,
-  scale = 1,
+  info,
+  scale = '1',
   edit,
+  onEditClick,
 }: hotSpotProps) => {
   return (
     <div
@@ -24,20 +28,21 @@ const Arrows = ({
       data-pitch={pitch}
       onClick={onClick}
     >
-      {/* <div className="  bottom-[180%] flex justify-center gap-2 left-[50%] translate-x-[-30%] text-xs p-2 text-black bg-white rounded border"> */}
       <div className="flex flex-col justify-between  text-xs items-center">
         <div className="flex items-center p-2 gap-2 bg-white rounded max-w-[200px]   whitespace-normal">
-          <span className=" break-words overflow-auto ">bedroom</span>
+          <span className=" break-words overflow-auto ">{info || ''}</span>
           {edit && (
             <EditIcon
-              className=""
-              sx={{ color: 'black', fontSize: '1rem' }}
-              onClick={(e) => e.stopPropagation()}
+              className=" w-full h-full hover:text-blue-500"
+              sx={{ color: 'black', fontSize: '1.2rem' }}
+              onClick={(e) => {
+                e.stopPropagation();
+                onEditClick && onEditClick();
+              }}
             />
           )}
         </div>
 
-        {/* </div> */}
         <div className="arrows-container flex items-center  ">
           <div
             style={{ transform: `scale(${scale})`, color: 'red' }}
