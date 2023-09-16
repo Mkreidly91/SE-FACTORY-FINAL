d98930cf691d31c54e1e70465f1f4bfd413ea4a7
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-16 20:29:31 +0300

    Frontend: restructured imageSlider component and changed some styling

--- Frontend/src/components/Common/ImageSlider/ImageSlider.tsx
+++ Frontend/src/components/Common/ImageSlider/ImageSlider.tsx
@@ -1,33 +1,48 @@
 import './imageSlider.css';
-const ImageSlider = ({
-  imageCardStyles,
-  images = [],
+const ImageCard = ({
+  url,
   onClick,
+  imageCardStyles,
 }: {
+  url: string;
   imageCardStyles?: string;
-  images: string[];
   onClick: () => void;
 }) => {
-  const ImageCard = ({
-    url,
-    onClick,
-  }: {
-    url: string;
-    onClick: () => void;
-  }) => {
-    return (
-      <div className={` inline-block px-3 ${imageCardStyles}`}>
-        <img
-          src={url}
-          className="w-[200px] h-auto aspect-video max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
-        />
-      </div>
-    );
-  };
   return (
-    <div className="flex overflow-x-scroll pt-10 pb-10 hide-scroll-bar">
-      <div className="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 ">
-        {images && images.map((e) => <ImageCard url={e} onClick={onClick} />)}
+    <div className={`inline-block `}>
+      <img
+        src={url}
+        className={`w-[200px] h-auto aspect-video max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out ${imageCardStyles}`}
+        onClick={() => onClick()}
+      />
+    </div>
+  );
+};
+
+const ImageSlider = ({
+  className,
+  imageCardStyles,
+  panoramas = [],
+  onClick,
+}: {
+  className: string;
+  imageCardStyles?: string;
+  panoramas: any[];
+  onClick: (id: string) => void;
+}) => {
+  return (
+    <div
+      className={`flex overflow-x-scroll pt-10 pb-10 hide-scroll-bar ${className}`}
+    >
+      <div className="flex gap-3 flex-nowrap lg:ml-40 md:ml-20 ml-10 ">
+        {panoramas &&
+          panoramas.map((e) => (
+            <ImageCard
+              imageCardStyles={imageCardStyles}
+              url={e.url}
+              onClick={() => onClick(e._id)}
+            />
+          ))}
       </div>
     </div>
   );
