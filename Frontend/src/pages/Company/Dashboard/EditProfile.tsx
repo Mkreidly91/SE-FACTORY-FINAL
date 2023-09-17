2597dddcc949047a5c293325e8fbce8dec58c27d
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-17 18:02:27 +0300

    Frontend: Added Edit profile page

---
+++ Frontend/src/pages/Company/Dashboard/EditProfile.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import EditProfileForm from '../../../components/Forms/EditProfileForm';
+import { getUserInfo } from '../../../api/user.api';
+
+const EditProfile = () => {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await getUserInfo();
+      console.log(res);
+      if (res) {
+        setUser(res);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  return (
+    <div className="mt-20 w-full p-5">
+      {user && <EditProfileForm initialValues={user} setUser={setUser} />}
+    </div>
+  );
+};
+
+export default EditProfile;
