41843ce9d23f1265de9ee47ef45d13f3c5c23c74
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-17 11:53:30 +0300

    Frontend: Added classname prop to UploadButton component

--- Frontend/src/components/Common/UploadButton.tsx
+++ Frontend/src/components/Common/UploadButton.tsx
@@ -2,13 +2,17 @@ const UploadButton = ({
   onChange,
   accept,
   text,
+  className,
 }: {
   accept: string;
   onChange: (e: any) => void;
   text: string;
+  className: string;
 }) => {
   return (
-    <label className=" bg-white px-2 py-3 rounded-md button-gradient text-white text-sm">
+    <label
+      className={` bg-white px-2 py-3 rounded-md button-gradient text-white text-sm ${className}`}
+    >
       <span> {text}</span>
       <input
         className=""
