1408da3c3426ba5e73e4ede8dadbae26e8006643
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-17 11:52:41 +0300

    Frontend: Added file upload for 3d model

--- Frontend/src/pages/Company/Dashboard/MarkersAndHotspots.tsx
+++ Frontend/src/pages/Company/Dashboard/MarkersAndHotspots.tsx
@@ -3,6 +3,8 @@ import ImageSlider from '../../../components/Common/ImageSlider/ImageSlider';
 import ObjectViewer from '../../sample3d/ObjectViewer';
 import V360 from '../../sample3d/V360.tsx';
 import PanoEditor from '../../sample3d/PanoEditor.tsx';
+import UploadButton from '../../../components/Common/UploadButton.tsx';
+import { addApartment } from '../../../api/company.api.ts';
 
 const MarkersAndHotspots = ({ initialState }) => {
   const objectUrl = initialState.url;
@@ -11,7 +13,16 @@ const MarkersAndHotspots = ({ initialState }) => {
     <Suspense>
       <div className="w-full ">
         <div className="mt-20 w-full">
-          <div className="mb-10">
+          <div className="mb-10 relative ">
+            <UploadButton
+              className="w-fit absolute left-[11%] top-[10px] cursor-pointer z-10 "
+              text={initialState.url ? 'Replace' : 'Add'}
+              accept="*"
+              onChange={async (e) => {
+                e.target.files[0] &&
+                  (await addApartment(initialState._id, e.target.files));
+              }}
+            />
             {objectUrl && <ObjectViewer url={initialState.url} />}
           </div>
 
