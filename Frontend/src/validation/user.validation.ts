c70974a2fbf1b695f8995b5a3447068d22ef1d97
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-18 19:03:46 +0300

    Frontend: Added validation for signup and login

--- Frontend/src/validation/user.validation.ts
+++ Frontend/src/validation/user.validation.ts
@@ -21,6 +21,30 @@ const editUserSchema = object({
     string().trim().url(),
   ]),
 }).partial();
+const signupFormSchema = object({
+  name: string().trim().min(1).max(18).nonempty('Name field is required'),
+  email: string().trim().email().nonempty('Email field is required'),
+  password: string().trim().min(6).nonempty('Password field is required'),
+  confirmPassword: string()
+    .trim()
+    .min(6)
+    .nonempty('Password field is required'),
+  userType: number().min(0).max(1),
+}).superRefine(({ confirmPassword, password }, ctx) => {
+  if (confirmPassword !== password) {
+    ctx.addIssue({
+      code: 'custom',
+      message: 'The passwords did not match',
+      path: ['confirmPassword'],
+    });
+  }
+});
+const loginFormSchema = object({
+  email: string().trim().email().nonempty('Email field is required'),
+  password: string().trim().min(6).nonempty('Password field is required'),
+});
 
 export type UserSchema = z.infer<typeof editUserSchema>;
-export { editUserSchema };
+export type SignupFormSchemaType = z.infer<typeof signupFormSchema>;
+export type LoginFormSchemaType = z.infer<typeof loginFormSchema>;
+export { signupFormSchema, loginFormSchema, editUserSchema };
