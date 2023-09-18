192cc494cbc87ad2eb777d56c81ea47472b855d5
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-18 19:04:36 +0300

    Frontend: Added apis for signup and login

---
+++ Frontend/src/api/auth.api.ts
@@ -0,0 +1,32 @@
+const baseURL = "http://localhost:8080/";
+import axios, { AxiosError } from "axios";
+import {
+  LoginFormSchemaType,
+  SignupFormSchemaType,
+} from "../validation/user.validation";
+import { handleError } from "./api.helpers";
+
+const signup = async (formData: SignupFormSchemaType) => {
+  try {
+    const res = await axios.post(`${baseURL}auth/register`, formData);
+    if (res.status === 200) {
+      return res.data.data;
+    }
+  } catch (error) {
+    const errors= error as Error | AxiosError;
+    return handleError(errors);
+  }
+};
+
+const login = async (formData: LoginFormSchemaType) => {
+  try {
+    const res = await axios.post(`${baseURL}auth/login`, formData);
+    if (res.status === 200) {
+      return res.data;
+    }
+  } catch (error) {
+    const errors= error as Error | AxiosError;
+    return handleError(errors);
+  }
+};
+export { signup, login };
