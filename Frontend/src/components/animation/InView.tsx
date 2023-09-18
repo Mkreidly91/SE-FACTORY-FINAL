52364b94e648262c8f022e261e55072f6e9e480e
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-18 11:02:20 +0300

    Frontend: created an inView animation component

---
+++ Frontend/src/components/animation/InView.tsx
@@ -0,0 +1,35 @@
+import { ReactNode, useRef } from 'react';
+import { useInView } from 'framer-motion';
+
+function InView({
+  children,
+  transform,
+  duration = '0.9',
+  delay = '0.5',
+  className,
+}: {
+  children: ReactNode;
+  transform: string;
+  duration?: string;
+  delay?: string;
+  className?: string;
+}) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  return (
+    <div
+      ref={ref}
+      className={className}
+      style={{
+        transform: isInView ? 'none' : `${transform}`,
+        opacity: isInView ? 1 : 0,
+        transition: `all ${duration}s cubic-bezier(0.17, 0.55, 0.55, 1) ${delay}s`,
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default InView;
