4d670c29644bcea9db644093b494c87ce9b88266
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-20 15:27:25 +0300

    Backend: Tested regex based search in searchProjectService

--- Backend/src/services/common.service.ts
+++ Backend/src/services/common.service.ts
@@ -1,3 +1,4 @@
+import { text } from 'body-parser';
 import { Project } from '../models/project';
 import { ProjectSearchSchema } from '../validation/common.validation';
 const searchProjectService = async ({
@@ -13,15 +14,20 @@ const searchProjectService = async ({
   const results = await Project.find(
     {
       // $text: { $search: new RegExp(search, 'i').source },
-      $text: { $search: search },
+      // $text: { $search: { index: 'text', regex: search } },
+      $or: [
+        { name: { $regex: search, $options: 'i' } },
+        { location: { $regex: search, $options: 'i' } },
+        { description: { $regex: search, $options: 'i' } },
+      ],
       bedrooms: { $lte: bedrooms },
       bathrooms: { $lte: bathrooms },
       size: { $gte: minSize, $lte: maxSize },
       price: { $gte: minPrice, $lte: maxPrice },
-    },
-    { score: { $meta: 'textScore' } }
+    }
+    // { score: { $meta: 'textScore' } }
   )
-    .sort({ score: { $meta: 'textScore' } })
+    // .sort({ score: { $meta: 'textScore' } })
     .exec();
 
   console.log(results);
