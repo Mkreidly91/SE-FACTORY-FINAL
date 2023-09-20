7bc5ac04890c6f051a3c8eabae0118f80dfc7a32
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-20 22:24:25 +0300

    Frontend : Added search and get project functions to common api

--- Frontend/src/api/common.api.ts
+++ Frontend/src/api/common.api.ts
@@ -1,14 +1,31 @@
-const baseURL = "http://localhost:8080/";
-import axios, { AxiosError } from "axios";
-import { handleError } from "./api.helpers";
-import { ProjectSearchFormSchemaType } from "../validation/project.validation";
+const baseURL = 'http://localhost:8080/';
+import axios, { AxiosError } from 'axios';
+import { handleError } from './api.helpers';
+import { ProjectSearchFormSchemaType } from '../validation/project.validation';
 
-const searchProject = async (formData: ProjectSearchFormSchemaType) => {
+const searchProject = async (
+  page = 1,
+  perPage = 10,
+  formData?: ProjectSearchFormSchemaType
+) => {
   try {
     const res = await axios.post(
-      `${baseURL}search/project`,
-      formData,
+      `${baseURL}search/project/${page}/${perPage}`,
+      formData
     );
+
+    if (res.status === 200) {
+      return res.data;
+    }
+  } catch (error) {
+    const errors = error as Error | AxiosError;
+    return handleError(errors);
+  }
+};
+
+const getAllProjects = async (page = 1, perPage = 10) => {
+  try {
+    const res = await axios.get(`${baseURL}getAllProjects/${page}/${perPage}`);
     if (res.status === 200) {
       return res.data.data;
     }
@@ -18,4 +35,4 @@ const searchProject = async (formData: ProjectSearchFormSchemaType) => {
   }
 };
 
-export { searchProject };
+export { searchProject, getAllProjects };
