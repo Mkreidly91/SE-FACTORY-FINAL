fae629f5d4702357d457753d7364783deb64553c
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-20 22:21:31 +0300

    Backend: Modified validation to handle pagination

--- Backend/src/validation/common.validation.ts
+++ Backend/src/validation/common.validation.ts
@@ -1,15 +1,36 @@
-import { string, number, object, z } from "zod";
+import { string, number, object, z } from 'zod';
+
 const projectSearchTerms = object({
   search: string().trim().optional(),
   location: string().trim().optional(),
-  bedrooms: number().positive("Please provide a valid number").optional(),
-  bathrooms: number().positive("Please provide a valid number").optional(),
-  size: number().array().length(2),
-  price: number().array().length(2),
+  bedrooms: number().positive('Please provide a valid number').optional(),
+  bathrooms: number().positive('Please provide a valid number').optional(),
+  size: number().array().length(2).optional(),
+  price: number().array().length(2).optional(),
 });
+
 const projectSearchForm = object({
   body: projectSearchTerms,
+  params: object({
+    page: string()
+      .transform((v) => parseInt(v))
+      .optional(),
+    perPage: string()
+      .transform((v) => parseInt(v))
+      .optional(),
+  }),
+});
+
+const getAllProjectsSchema = object({
+  params: object({
+    page: string()
+      .transform((v) => parseInt(v))
+      .optional(),
+    perPage: string()
+      .transform((v) => parseInt(v))
+      .optional(),
+  }),
 });
 
 export type ProjectSearchSchema = z.infer<typeof projectSearchTerms>;
-export { projectSearchForm };
+export { projectSearchForm, getAllProjectsSchema };
