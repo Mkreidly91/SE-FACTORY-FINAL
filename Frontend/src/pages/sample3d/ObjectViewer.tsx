5ddc9112a45cc5a2fa54b099489ff53983e84bdb
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-22 14:43:24 +0300

    Frontend: Added Bounds component for auto resizing in 3d object viewer component

--- Frontend/src/pages/sample3d/ObjectViewer.tsx
+++ Frontend/src/pages/sample3d/ObjectViewer.tsx
@@ -1,25 +1,66 @@
-import { Html, OrbitControls, useGLTF } from '@react-three/drei';
+import {
+  Bounds,
+  useBounds,
+  Html,
+  OrbitControls,
+  useGLTF,
+} from '@react-three/drei';
 import { Canvas, ThreeEvent } from '@react-three/fiber';
-import React, { useState, useEffect, FC, Suspense, useRef } from 'react';
+import React, {
+  useState,
+  useEffect,
+  FC,
+  Suspense,
+  useRef,
+  useMemo,
+} from 'react';
 import * as THREE from 'three';
 
 export const ObjectViewer = ({ url }: { url: string }) => {
   const m: any = useGLTF(url);
-  const aspect = window.innerWidth / window.innerHeight;
-  const d = 60;
-  const camera = new THREE.OrthographicCamera(
-    -d * aspect,
-    d * aspect,
-    d,
-    -d,
-    1,
-    1000
-  );
-  camera.position.set(60, 60, 60);
-  camera.zoom = 50;
 
+  const model = useRef();
+  const camera = useMemo(() => {
+    const aspect = window.innerWidth / window.innerHeight;
+    const d = 60;
+    const camera = new THREE.OrthographicCamera(
+      -d * aspect,
+      d * aspect,
+      d,
+      -d,
+      1,
+      1000
+    );
+    camera.position.set(60, 60, 60);
+    camera.zoom = 50;
+
+    return camera;
+  }, [m.scene]);
+  const bounds = useBounds();
+  function SelectToZoom({
+    children,
+  }: {
+    children: JSX.Element | JSX.Element[];
+  }) {
+    const api = useBounds();
+    return (
+      <group
+        onClick={(e) => (
+          e.stopPropagation(), e.delta <= 2 && api.refresh(e.object).fit()
+        )}
+        onPointerMissed={(e) => e.button === 0 && api.refresh().fit()}
+      >
+        {children}
+      </group>
+    );
+  }
+  useEffect(() => {
+    if (model?.current) {
+      bounds?.refresh();
+    }
+  }, [model?.current]);
   return (
-    <div className="w-[80%] h-[500px] mx-auto ">
+    <>
       {m && (
         <Canvas
           className=" border border-black h-full bg-transparent rounded-md"
@@ -29,7 +70,6 @@ export const ObjectViewer = ({ url }: { url: string }) => {
         >
           <ambientLight intensity={0.1} />
           <directionalLight color="white" position={[0, 0, 5]} intensity={1} />
-          {/* <directionalLight color="white" position={[5, 0, 5]} /> */}
           <pointLight intensity={2} position={[-10, 30, 20]} castShadow />
           <pointLight
             intensity={1}
@@ -37,24 +77,18 @@ export const ObjectViewer = ({ url }: { url: string }) => {
             castShadow
             visible={true}
           />
-          {/* <spotLight color="yellow" position={[0, 0, 0]} /> */}
-
           <OrbitControls makeDefault target={[0, 2, 0]} />
+
           <Suspense>
-            {m && (
-              <primitive
-                onPointerDown={(e) => {
-                  //   placeMarker(e);
-                }}
-                object={m.scene}
-              >
-                {/* {children} */}
-              </primitive>
+            {m.scene && (
+              <Bounds fit clip observe margin={1.2} damping={1}>
+                <primitive scale={1} ref={model} object={m.scene} />
+              </Bounds>
             )}
           </Suspense>
         </Canvas>
       )}
-    </div>
+    </>
   );
 };
 
