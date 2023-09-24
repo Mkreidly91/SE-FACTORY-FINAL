0b1d1ef7bde266e683b1412395f2e6b9b3211f46
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-24 16:05:31 +0300

    Frontend: Minor changes to signUpform, replaced button with material tailwind button

--- Frontend/src/components/Forms/SignupForm.tsx
+++ Frontend/src/components/Forms/SignupForm.tsx
@@ -1,26 +1,27 @@
-import { Button, TextField } from "@mui/material";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { TextField } from '@mui/material';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from '@material-tailwind/react';
 
-import { useNavigate } from "react-router-dom";
-import { signup } from "../../api/auth.api";
+import { Link, useNavigate } from 'react-router-dom';
+import { signup } from '../../api/auth.api';
 import {
   SignupFormSchemaType,
   signupFormSchema,
-} from "../../validation/user.validation";
-import { useState } from "react";
-import { ApiError } from "../../api/api.helpers";
-import ApiErrorHandler from "../Common/ApiError";
+} from '../../validation/user.validation';
+import { useState } from 'react';
+import { ApiError } from '../../api/api.helpers';
+import ApiErrorHandler from '../Common/ApiError';
 
-const SignupForm = () => {
+const SignupForm = ({ className }: { className?: string }) => {
   const navigate = useNavigate();
   const [err, setError] = useState<ApiError | null>(null);
   const onSubmit: SubmitHandler<SignupFormSchemaType> = async (data) => {
-    const res = await signup(data);
-    if (res.error) {
-      setError(res.error);
+    const { status, error } = await signup(data);
+    if (error) {
+      setError(error);
     } else {
-      navigate("/login");
+      navigate('/login');
     }
   };
 
@@ -31,35 +32,34 @@ const SignupForm = () => {
     formState: { errors, isSubmitting, isValid },
   } = useForm<SignupFormSchemaType>({
     resolver: zodResolver(signupFormSchema),
-    mode: "onTouched",
-    criteriaMode: "firstError",
+    mode: 'onTouched',
+    criteriaMode: 'firstError',
     defaultValues: {
-      name: "",
-      email: "",
-      password: "",
-      confirmPassword: "",
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
       userType: 0,
     },
   });
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex gap-4 w-full h-full justify-around  mt-20"
-    >
-      <div className="project-form-left w-[50%] flex flex-col gap-1 items-center ">
-      <ApiErrorHandler error={err}/>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div
+        className={`project-form-left flex flex-col gap-1 items-center ${className}`}
+      >
+        <ApiErrorHandler error={err} />
         <div className="flex flex-col gap-3 w-full justify-center">
-          <label className="font-semibold">Full Name</label>
+          <label className="font-semibold">Name</label>
           <TextField
             className="max-w-[600px]"
             autoFocus
             required
-            InputLabelProps={{ className: "" }}
-            onFocus={() => clearErrors("name")}
+            InputLabelProps={{ className: '' }}
+            onFocus={() => clearErrors('name')}
             error={Boolean(errors.name)}
-            helperText={errors?.name?.message || " "}
-            {...register("name")}
+            helperText={errors?.name?.message || ' '}
+            {...register('name')}
           />
         </div>
 
@@ -68,10 +68,10 @@ const SignupForm = () => {
           <TextField
             className="max-w-[600px]"
             required
-            onFocus={() => clearErrors("email")}
+            onFocus={() => clearErrors('email')}
             error={Boolean(errors.email)}
-            helperText={errors?.email?.message || " "}
-            {...register("email")}
+            helperText={errors?.email?.message || ' '}
+            {...register('email')}
           />
         </div>
 
@@ -79,13 +79,13 @@ const SignupForm = () => {
           <label className="font-semibold">Password</label>
           <TextField
             className="max-w-[600px]"
-            InputLabelProps={{ className: "" }}
+            InputLabelProps={{ className: '' }}
             required
             type="password"
-            onFocus={() => clearErrors("password")}
+            onFocus={() => clearErrors('password')}
             error={Boolean(errors.password)}
-            helperText={errors?.password?.message || " "}
-            {...register("password")}
+            helperText={errors?.password?.message || ' '}
+            {...register('password')}
           />
         </div>
 
@@ -95,39 +95,48 @@ const SignupForm = () => {
             className="max-w-[600px]"
             required
             type="password"
-            InputLabelProps={{ className: "" }}
-            onFocus={() => clearErrors("confirmPassword")}
+            InputLabelProps={{ className: '' }}
+            onFocus={() => clearErrors('confirmPassword')}
             error={Boolean(errors.confirmPassword)}
-            helperText={errors?.confirmPassword?.message || " "}
-            {...register("confirmPassword")}
+            helperText={errors?.confirmPassword?.message || ' '}
+            {...register('confirmPassword')}
           />
         </div>
 
-        <div className="flex flex-col gap-3 w-full justify-center">
+        {/* <div className="flex flex-col gap-3 w-full justify-center">
           <label className="font-semibold">User Type</label>
           <TextField
             className="max-w-[600px]"
             type="number"
-            InputLabelProps={{ className: "" }}
-            onFocus={() => clearErrors("userType")}
+            InputLabelProps={{ className: '' }}
+            onFocus={() => clearErrors('userType')}
             error={Boolean(errors.userType)}
-            helperText={errors?.userType?.message || " "}
-            {...register("userType", { valueAsNumber: true })}
+            helperText={errors?.userType?.message || ' '}
+            {...register('userType', { valueAsNumber: true })}
           />
+        </div> */}
+
+        <div className=" w-full project-form-buttons flex justify-between items-center gap-3">
+          <Button
+            type="submit"
+            disabled={isSubmitting || !isValid}
+            variant={`gradient`}
+            className="light-gradient p-4 font-sora font-normal uppercase tracking-wider px-10 cursor-pointer disabled  "
+            size="lg"
+          >
+            Sign up
+          </Button>
+          <div className="text-sm ">
+            Have an account? Sign in{' '}
+            <Link to="/signIn">
+              <a className="text-sky-700">here</a>
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="project-form-buttons flex justify-center gap-3s">
-        <Button
-          color="inherit"
-          className="bg-red-200 text-red-200 font-extrabold"
-          type="submit"
-          disabled={isSubmitting || !isValid}
-        >
-          Submit
-        </Button>
       </div>
     </form>
   );
 };
 
 export default SignupForm;
+// disabled:bg-gradient-to-r disabled:from-gray-900 disabled:to-gray-500
