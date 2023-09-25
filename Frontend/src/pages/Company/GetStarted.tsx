b47c00ad4f70a2299f8789d3e7ed79576144999c
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-25 14:30:54 +0300

    Frontend: Revamped GetStarted page

--- Frontend/src/pages/Company/GetStarted.tsx
+++ Frontend/src/pages/Company/GetStarted.tsx
@@ -2,29 +2,29 @@ import Navbar from '../../components/Navbar';
 import InView from '../../components/animation/InView';
 import ObjectViewer from '../sample3d/ObjectViewer';
 import V360 from '../sample3d/V360';
-import Button from '../../components/Common/Button';
 import { NewObjectViewer } from '../sample3d/NewObjectViewer';
 import * as THREE from 'three';
+import { Suspense } from 'react';
 
 import villa1 from '../../assets/images/get-started/villa1.png';
 import villa2 from '../../assets/images/get-started/villa2.png';
 import villa3 from '../../assets/images/get-started/Rectangle32.png';
 import villa4 from '../../assets/images/get-started/Rectangle34.png';
 import villa5 from '../../assets/images/get-started/Rectangle32-1.png';
-import villa6 from '../../assets/images/get-started/Rectangle34.png';
+import villa6 from '../../assets/images/get-started/Rectangle34-1.png';
 import Footer from '../../components/Footer';
 import { Link } from 'react-router-dom';
-import { Suspense } from 'react';
+import { Button } from '@material-tailwind/react';
 
 const GetStarted = () => {
   return (
-    <>
+    <div id="top">
       <section className="hero h-[100vh] relative bg-black">
         <div className="overlay absolute w-full h-full  bg-black opacity-60  z-10"></div>
         <Navbar className="  w-full z-[20] relative" />
         <InView
           transform="translateY(100px)"
-          className="absolute top-0 left-0 bg-transparent  flex flex-col items-center justify-center w-full h-full  z-10 text-white  "
+          className="absolute top-0 left-0 bg-transparent flex flex-col items-center justify-center w-full h-full  z-10 text-white  "
         >
           <span className="hero-text-center">Unlock new possibilities</span>
         </InView>
@@ -37,100 +37,89 @@ const GetStarted = () => {
           />
         </Suspense>
       </section>
-      <section className="py-10  page-h">
-        <div className="text-center font-semibold  text-2xl md:text-4xl pb-10">
+      <section className="py-10 page-h">
+        <div className="font-semibold text-xl md:text-4xl text-center pb-20">
           Why List With Us?
         </div>
-        <div className="  flex  justify-center  items-center  text-center  ">
-          <div className="exposure  flex flex-col items-start justify-center text-start   w-[50%]">
-            <span className=" text-xl md:text-3xl font-semibold ">
+        <div className="flex flex-col md:flex-row items-center justify-center  w-11/12 md:w-full mx-auto text-center font-sora md:text-lg text-base font-light tracking-wide">
+          <div className="exposure flex flex-col items-center gap-5 md:w-1/2 w-full text-center md:text-left">
+            <span className="font-semibold w-8/12 text-xl md:text-3xl ">
               Unparalleled Exposure
             </span>
-            <br />
-            <span className="  w-[60%] hidden md:block">
+
+            <span className="md:w-8/12 font-sora text-base font-light">
               Gain access to a vast audience of potential clients, investors,
               and partners actively seeking construction services. Our platform
               is designed to put your projects in the spotlight.
             </span>
           </div>
-          <div className="w-1/2 flex  ">
-            <div className="img-container flex justify-end gap-5">
-              <img
-                src={villa1}
-                className="grow w-1/2 max-w-[300px] max-h-full object-cover rounded-md"
-                alt=""
-              />
-
-              <img
-                src={villa2}
-                className="grow w-1/2 max-w-[200px] object-cover mt-5 translate-y-5  rounded-md"
-                alt=""
-              />
-            </div>
+          <div className="flex justify-center items-center space-x-2 w-1/2 grow md:gap-2 md:space-x-0 md:mt-5">
+            <img
+              src={villa1}
+              className="grow w-1/2 max-w-[350px] max-h-full object-cover rounded-md -translate-y-4"
+              alt=""
+            />
+            <img
+              src={villa2}
+              className=" w-1/2 max-w-[150px] object-cover mt-5 rounded-md"
+              alt=""
+            />
           </div>
         </div>
       </section>
 
-      <section className="py-10  page-h bg-gray-800">
-        <div className="  flex  justify-center  items-center  text-center  ">
-          <div className="w-1/2  flex  ">
-            <div className="img-container flex gap-5">
-              <img
-                src={villa3}
-                className="grow w-1/2 max-w-[300px] max-h-full object-cover rounded-md"
-                alt=""
-              />
-              <img
-                src={villa4}
-                className="w-1/2 max-w-[200px] object-cover mt-5 translate-y-5  rounded-md"
-                alt=""
-              />
-            </div>
+      <section className="py-10  page-h bg-gradient-to-r from-neutral-800 to-neutral-950 ">
+        <div className="flex flex-col md:flex-row items-center justify-center  w-11/12 md:w-full mx-auto text-center font-sora md:text-lg text-base font-light tracking-wide text-white">
+          <div className="flex justify-center items-center space-x-2 w-1/2 grow md:gap-2 md:space-x-0 md:mt-5">
+            <img
+              src={villa3}
+              className="grow w-1/2 max-w-[350px] max-h-full object-cover rounded-md -translate-y-4"
+              alt=""
+            />
+            <img
+              src={villa4}
+              className=" w-1/2 max-w-[150px] object-cover mt-5 rounded-md"
+              alt=""
+            />
           </div>
-          <div className="exposure flex items-center justify-end text-start text-white   max-w-[50%]">
-            <div className="flex flex-col  w-fit">
-              <span className=" text-xl md:text-3xl font-semibold ">
-                Cost-Effective Marketing
-              </span>
-              <br />
-              <span className="hidden md:block  w-[60%] ">
-                Maximize your marketing budget by harnessing the power of our
-                platform. We offer cost-effective advertising options to reach a
-                wide and relevant audience.
-              </span>
-            </div>
+          <div className="exposure flex flex-col items-center gap-5 md:w-1/2 w-full text-center md:text-left">
+            <span className="font-semibold w-8/12 text-xl md:text-3xl ">
+              Cost-Effective Marketing
+            </span>
+
+            <span className="md:w-8/12 font-sora text-base font-light">
+              Maximize your marketing budget by harnessing the power of our
+              platform. We offer cost-effective advertising options to reach a
+              wide and relevant audience.
+            </span>
           </div>
         </div>
       </section>
 
       <section className="py-10  page-h">
-        <div className="  flex  justify-center  items-center  text-center  ">
-          <div className="exposure  flex flex-col items-start justify-center text-start   w-[50%] container">
-            <span className=" text-xl md:text-3xl font-semibold ">
+        <div className="flex flex-col md:flex-row items-center justify-center  w-11/12 md:w-full mx-auto text-center font-sora md:text-lg text-base font-light tracking-wide">
+          <div className="exposure flex flex-col items-center gap-5 md:w-1/2 w-full text-center md:text-left">
+            <span className="font-semibold w-8/12 text-xl md:text-3xl ">
               Stay Ahead of the Curve
             </span>
-            <br />
-            <span className="  w-[60%] hidden md:block">
+
+            <span className="md:w-8/12 font-sora text-base font-light">
               Stay updated on industry trends, best practices, and emerging
               technologies through our resourceful content and community
               discussions. Position your business as an industry pioneer.
             </span>
           </div>
-
-          <div className="w-1/2 flex  ">
-            <div className="img-container flex justify-end gap-5">
-              <img
-                src={villa5}
-                className="grow w-1/2 max-w-[300px] max-h-full object-cover rounded-md"
-                alt=""
-              />
-
-              <img
-                src={villa6}
-                className="grow w-1/2 max-w-[200px] object-cover mt-5 translate-y-5  rounded-md"
-                alt=""
-              />
-            </div>
+          <div className="flex justify-center items-center space-x-2 w-1/2 grow md:gap-2 md:space-x-0 md:mt-5">
+            <img
+              src={villa5}
+              className="grow w-1/2 max-w-[350px] max-h-full object-cover rounded-md -translate-y-4"
+              alt=""
+            />
+            <img
+              src={villa6}
+              className=" w-1/2 max-w-[150px] object-cover mt-5 rounded-md"
+              alt=""
+            />
           </div>
         </div>
       </section>
@@ -140,14 +129,19 @@ const GetStarted = () => {
             Want to learn more?
           </span>
 
-          <Button
-            text="Get Started"
-            className=" button-gradient button-large-responsive"
-          />
+          <Link to="/Login">
+            <Button
+              variant="gradient"
+              className="bg-gradient-to-r from-slate-700 to-slate-500 p-4 font-sora font-normal uppercase tracking-wider px-10"
+              size="lg"
+            >
+              Get Started
+            </Button>
+          </Link>
         </div>
       </section>
       <Footer />
-    </>
+    </div>
   );
 };
 
