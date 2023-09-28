c249963985b06366e778487c906356edb3e7edee
Mostafa Kreidly <mkreidly91@gmail.com>
2023-09-28 10:02:39 +0300

    Frontend: Added link to footer

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -1,18 +1,19 @@
 import logo from '../assets/icons/logo/logo-full-black.svg';
-import Button from './Common/Button';
-
 import facebook from '../assets/icons/socials/Facebook.svg';
 import twitter from '../assets/icons/socials/Twitter.svg';
 import instagram from '../assets/icons/socials/Instagram.svg';
 import linkedIn from '../assets/icons/socials/LinkedIn.svg';
 import yt from '../assets/icons/socials/YouTube.svg';
+import { Link } from 'react-router-dom';
 
 const Footer = ({ className }: { className?: string }) => {
   return (
     <div
       className={`flex justify-between items-center py-6 px-8 md:py-8 md:px-10 text-white bg-transparent z-10 ${className}`}
     >
-      <img src={logo} alt="" className=" w-[75px] md:w-[100px] " />
+      <Link to="/">
+        <img src={logo} alt="" className=" w-[75px] md:w-[100px] " />
+      </Link>
       <div className="socials flex gap-5">
         <img
           src={facebook}
